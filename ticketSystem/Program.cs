f7971f66f564ee270fa1b6683b37f52c8f6f8375
Sereny Sanchez <52845840+SerenyS@users.noreply.github.com>
2020-01-27 20:47:16 -0600

    Add project files.

---
+++ ticketSystem/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ticketSystem
+{
+    static class Tickets
+    {
+        public static void Main(string[] args)
+        {
+            string file = "tickets.csv";
+            string selection;
+
+            Console.WriteLine("1.Enter new ticket");
+            Console.WriteLine("2.Read current tickets");
+            Console.WriteLine("3.Exit Program");
+
+            selection = Console.ReadLine();
+
+            if (selection == "1")
+            {
+                string TicketId;
+                string Summary;
+                string Status;
+                string Prioty;
+                string Submitter;
+                string Assigned;
+                string Watching;
+
+         
+                Console.WriteLine("What is the Ticket Id?");
+                TicketId = Console.ReadLine();
+
+                Console.WriteLine("What is this ticket for?");
+                Summary = Console.ReadLine();
+
+
+              
+            }
+            else if (selection == "2")
+            {
+
+            }
+          else {
+
+            }
+        }
+
+    }
+}
